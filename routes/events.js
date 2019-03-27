3df065f9599e9e26b3483ffd723466d67874da1b
Hector Flores <hecto932@gmail.com>
2019-03-27 17:41:21 -0600

    CSV

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,9 @@
 const debug = require('debug')('api-events:event-router');
 const express = require('express');
+const js2xmlparser = require('js2xmlparser');
 const EventService = require('../services/events');
+const { Parser } = require('json2csv');
+
 
 function eventsApi(app) {
   const router = express.Router();
@@ -12,7 +15,25 @@ function eventsApi(app) {
     debug(`A request has come to /events`);
     const { query } = req;
 
-    const events = eventService.get(query);
+    let events = eventService.get(query);
+
+    events = query && query.limit ? events.slice(0, query.limit) : events;
+    events = query && query.format && query.format === 'xml' ? js2xmlparser.parse('event', events) : events;
+    
+    if (query && query.format && query.format === 'xml') {
+      res.setHeader('Content-Type', 'text/xml');
+      return res.status(200).send(events);
+    }
+
+    if (query && query.format && query.format === 'csv') {
+      const fields = ['uuid', 'name', 'reporter', 'description', 'type', 'location', 'datetime'];
+      const opts = { fields };
+      const parser = new Parser(opts);
+      const csv = parser.parse(events);
+      res.setHeader('Content-disposition', 'attachment; filename=testing.csv');
+      res.set('Content-Type', 'text/csv');
+      return res.status(200).send(csv);
+    }
 
     res.status(200).json({
       error: false,
